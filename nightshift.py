52813193f321766b91a0127d141097dedd1a0834
shadowthebat <10837037+shadowthebat@users.noreply.github.com>
2019-01-23 19:56:37 -0500

    implement loading time

--- nightshift.py
+++ nightshift.py
@@ -1,13 +1,15 @@
 import pyautogui
+from time import sleep
 # Script that toggles nightshift mode on my mac
 
-pyautogui.PAUSE = 1 # waits one second inbetween clicks
+pyautogui.PAUSE = 1.25 # waits x second(s) inbetween clicks
 pyautogui.FAILSAFE = True # breaks script uppon moving pointer to upperleft screen
 
 # click the apple
 pyautogui.click(x=29, y=15, clicks=1, button='left')
 # System Preferences
 pyautogui.click(x=74, y=64, clicks=1, button='left')
+sleep(0.5) # Allows loading time for SysPref
 # Displays
 pyautogui.click(x=427, y=304, clicks=1, button='left')
 # Night Shift
